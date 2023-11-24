7893ab066a1941a196e38e004805080191594872
Nihan Samnani <nihansamnani@gmail.com>
2023-11-24 17:05:15 +0530

    Project Animation

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-// gsap.registerPlugin(ScrollTrigger);
 
-// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector(".main"),
-//   smooth: true
-// });
-// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-// locoScroll.on("scroll", ScrollTrigger.update);
-
-// // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-// ScrollTrigger.scrollerProxy(".main", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-//   pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-// });
-
-
-// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-// ScrollTrigger.refresh();
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
@@ -56,9 +28,7 @@ function loaderAni(){
         display: "none",
         opacity: 0,
         duration: 1
-
     })
-    
 }
 
 function circleFollow(){
@@ -66,7 +36,8 @@ function circleFollow(){
         gsap.to(".circle", {
             scale: 1,
             x: dets.x,
-            y: dets.y
+            y: dets.y,
+            delay: 0
         })
     })
 }
@@ -88,6 +59,77 @@ function heroAni(){
         duration: 1
     })
 }
+function proAni(){
+    document.querySelector(".project1").addEventListener("mousemove", function(){
+        gsap.to(".circle", {
+            color: "white",
+            backgroundColor: "black",
+            height: "5rem",
+            width: "5rem"
+        })
+       
+    })
+    document.querySelector(".project1").addEventListener("mouseleave", function(){
+        gsap.to(".circle", {
+            color: "transparent",
+            backgroundColor: "white",
+            height: "1rem",
+            width: "1rem"
+        })
+        
+    })
+    document.querySelector(".project1").addEventListener("mousemove", function(){
+        
+        gsap.to(".p-box",{
+            height: "50%",
+            width: "50%",
+            duration: 1
+        })
+    })
+    document.querySelector(".project1").addEventListener("mouseleave", function(){
+        gsap.to(".project1 img", {
+            opacity: 1
+        })
+        gsap.to(".p-box",{
+            height: "0%",
+            width: "0%"
+        })
+    })
+    document.querySelector(".project2").addEventListener("mousemove", function(){
+        gsap.to(".circle", {
+            color: "black",
+            backgroundColor: "white",
+            height: "5rem",
+            width: "5rem"
+        })
+    })
+    document.querySelector(".project2").addEventListener("mouseleave", function(){
+        gsap.to(".circle", {
+            color: "transparent",
+            backgroundColor: "white",
+            height: "1rem",
+            width: "1rem"
+        })
+    })
+    document.querySelector(".project3").addEventListener("mousemove", function(){
+        gsap.to(".circle", {
+            color: "white",
+            backgroundColor: "black",
+            height: "5rem",
+            width: "5rem"
+        })
+    })
+    document.querySelector(".project3").addEventListener("mouseleave", function(){
+        gsap.to(".circle", {
+            color: "transparent",
+            backgroundColor: "white",
+            height: "1rem",
+            width: "1rem"
+        })
+    })
+    
+}
 loaderAni();
-circleFollow();
+// circleFollow();
+proAni();
 // heroAni();
